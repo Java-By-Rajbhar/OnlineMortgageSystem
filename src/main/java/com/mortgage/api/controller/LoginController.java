ff4b2c8d91ebc5afdbadcd4eabdb35e6aae29781
User1 <User1@LP-5CD9176M6P.HCLT.CORP.HCL.IN>
2019-09-03 15:24:21 +0530

    Added Login

--- src/main/java/com/mortgage/api/controller/LoginController.java
+++ src/main/java/com/mortgage/api/controller/LoginController.java
@@ -1,8 +1,19 @@
 package com.mortgage.api.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mortgage.api.dto.LoginRequestDto;
+import com.mortgage.api.dto.LoginResponseDto;
+import com.mortgage.api.service.LoginService;
 
 /**
  * 
@@ -13,5 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(allowedHeaders = { "*", "/" }, origins = { "*", "/" })
 public class LoginController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+	@Autowired
+	LoginService loginService;
+
+	@PostMapping("/login")
+	public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+		LOGGER.info("inside MORTGAGE LOGIN");
+		LoginResponseDto loginResponseDto = loginService.login(loginRequestDto);
+		return new ResponseEntity<>(loginResponseDto, HttpStatus.OK);
+	}
 
 }
