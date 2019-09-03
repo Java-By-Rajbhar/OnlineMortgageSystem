7af5377bbf2abd57449ba6e78d5c1c42fc599101
user1 <anuradha.krishnanov7@gmail.com>
2019-09-03 18:31:44 +0530

    added last

--- src/test/java/com/mortgage/api/service/LoginServiceimplTest.java
+++ src/test/java/com/mortgage/api/service/LoginServiceimplTest.java
@@ -5,14 +5,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class LoginServiceimplTest {
 
-	@Test
-	public void testLogin() {
-		fail("Not yet implemented");
-	}
-
-=======
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
